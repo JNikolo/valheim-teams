fb3d2b768c720d1a5d757639934af07364c390b0
JNikolo <jaircoto007@gmail.com>
2025-11-23 23:14:08 -0500

    add set of possible matches to look

--- server/main.py
+++ server/main.py
@@ -1,10 +1,17 @@
 from typing import Union
 from fastapi import FastAPI
-from valheim_save_tools_py import ValheimSaveTools
+from valheim_save_tools_py import ValheimSaveTools, parse_items_from_base64
 
 app = FastAPI()
 
 vst = ValheimSaveTools(verbose=True)
+
+CHEST_PREFABS = {
+    "piece_chest",
+    "piece_chest_wood",
+    "piece_chest_iron",
+    "piece_chest_blackmetal",
+}
 
 @app.get("/")
 def read_root():
@@ -18,20 +25,19 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.get("/parse_save/")
 def parse_save(file_path: str = ""):
     try:
-        chest_prefabs = [
-            "piece_chest",
-            "piece_chest_wood",
-            "piece_chest_iron",
-            "piece_chest_blackmetal",
-            "piece_chest_stone"
-        ]
         save_data = vst.to_json(file_path)
 
         zdoList = save_data.get("zdoList", [])
 
-        chest_data = [zdo for zdo in zdoList if zdo.get("prefabName", "") in chest_prefabs]
+        chest_data = [zdo for zdo in zdoList if zdo.get("prefabName", "") in CHEST_PREFABS]
 
-        items = [chest.get('stringsByName', {}).get('items', '') for chest in chest_data]
+        items = []
+
+        for chest in chest_data:
+            chestStringByName = chest.get('stringsByName', {})
+            chestItemsString = chestStringByName.get('items', '')
+            chest_items = parse_items_from_base64(chestItemsString)
+            items.append(chest_items)
         
         return {"status": "success", "total_chests": len(chest_data), "total_items": len(items), "items": items}
     except Exception as e:
